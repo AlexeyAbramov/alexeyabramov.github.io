c8dbfd6cbddc8cb01874894a14ae223ec01ba2b7
AlexeyAbramov <abramov.alexey.93@ya.ru>
2019-09-26 01:28:43 +0300

    commit

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const git = currentUrl("git");
 const vk = currentUrl("vk");
+const insta = currentUrl("insta");
 const portfolio = currentUrl();
 const twitch = currentUrl("tw-app");
 const goodCarton = currentUrl("good-carton");
 
 
 function currentUrl(url) {
-    let element = document.getElementsByClassName("kek")[1];
-    return function () {
-        if (url === "git") {
-            gitUrl(element);
-        } else if (url === "vk") {
-            vkUrl(element);
-        } else
-        return element.href = `https://alexeyabramov.github.io/${url}`;
+
+    let element = document.getElementById(`${url}`);
+    if (url === "git") {
+        gitUrl(element);
+    } else if (url === "vk") {
+        vkUrl(element);
+    } else if (url === "insta") {
+        instaUrl(element)
+    } else if (url === "tw-app") {
+        instaUrl(element)
+    } else if (url === "good-carton") {
+        instaUrl(element)
     }
+        return function () {
+            element.href = `https://alexeyabramov.github.io/${url}`;
+        }
 }
 
 function gitUrl(e) {
@@ -23,4 +31,8 @@ function gitUrl(e) {
 
 function vkUrl(e) {
     return e.href = "https://vk.com/id20692837";
+}
+
+function instaUrl(e) {
+    return e.href = "https://instagram.com/al.abram0v";
 }
